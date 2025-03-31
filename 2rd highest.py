cccf912719f195cd24a84a3dd52d19ece80347ad
Tony27032007 <Tonymasteri2703@gmail.com>
2025-04-01 09:15:17 +1100

    cube is done but not 2h

--- 2rd highest.py
+++ 2rd highest.py
@@ -5,7 +5,8 @@ while n<2:
 int(n)
 for i in range (i,n):
       l.append(int(input("Enter a number: ")))
-      for i in range (i,n):
-            if l[i]>l[i+1]:
-                  l[i]=l[i+1] and l[i+1]=l[i]:
+
+for i in range (i,n):
+      if l[i]>l[i+1]:
+            l[i]=l[i+1] and l[i+1]=l[i]
 print("The second highest number is:", l[n-1])
---
+++ cube.py
@@ -0,0 +1,4 @@
+n=input("give me a number")
+n=int(n)
+cube=lambda n:n*n*n
+print(cube(n))
