56392fc6c5d2b2da9f79fdbb3a12314d4c906b92
Tony27032007 <Tonymasteri2703@gmail.com>
2025-04-01 09:05:12 +1100

    2h on the way

---
+++ 2rd highest.py
@@ -0,0 +1,11 @@
+l=[]
+n=input("How many number in the list: ")
+while n<2:
+      print("list must have more than 2")
+int(n)
+for i in range (i,n):
+      l.append(int(input("Enter a number: ")))
+      for i in range (i,n):
+            if l[i]>l[i+1]:
+                  l[i]=l[i+1] and l[i+1]=l[i]:
+print("The second highest number is:", l[n-1])
