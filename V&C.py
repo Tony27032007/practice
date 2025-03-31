59f399fe99993bd48d24ff71bb7b9f034382ab45
Tony27032007 <Tonymasteri2703@gmail.com>
2025-04-01 08:46:52 +1100

    V&C is done

--- V&C.py
+++ V&C.py
@@ -1,10 +1,10 @@
-v=[a, e, i, o, u]
+v=["a", "e", "i", "o", "u"]
 w=input("Enter a word: ")
 n=len(w)
 i=0
 x=0
 y=0
-while i<w:
+while i<n:
       if w[i] in v:
             x=x+1
             i=i+1
