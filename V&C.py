dec8c4903da67ffde0144765069c5533c6b0d81b
Tony27032007 <Tonymasteri2703@gmail.com>
2025-04-01 08:42:56 +1100

    Doing V&C

---
+++ V&C.py
@@ -0,0 +1,15 @@
+v=[a, e, i, o, u]
+w=input("Enter a word: ")
+n=len(w)
+i=0
+x=0
+y=0
+while i<w:
+      if w[i] in v:
+            x=x+1
+            i=i+1
+      else:
+            y=y+1
+            i=i+1
+print("Vowels:", x)
+print("Consonants:", y)
---
+++ divide 4.py
@@ -0,0 +1,6 @@
+n=input("Enter a number")
+n=int(n)
+if n%4==0:
+      print("The number is divisible by 4")
+else:
+      print("The number is not divisible by 4")
